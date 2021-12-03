43635bf8dbbad4320b5767b2d951ba78dc4af63a
era <the@communist.dev>
2021-12-03 10:54:51 +0000

    install script

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-apt-get install alacritty fd-find
+apt-get install neovim zsh git fd-find bat nodejs npm rustc golang build-tools
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir -p $HOME/.config/nvim
+ln -s $HOME/programming/.functions/init.vim $HOME/.config/nvim/init.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim +'PlugInstall' +qa --headless
+
+rm $HOME/.zshrc
+
+ln -s $HOME/programming/.functions/.zshrc  $HOME/.zshrc
+
+# Desktop only
+apt-get install alacritty
